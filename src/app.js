77aa6cce8407ee670c06a41280cb914bd6ffd061
Marcelo <marcelo.bohn@consyste.com.br>
2016-10-14 11:23:46 -0300

    yeoman structure

---
+++ src/app.js
@@ -0,0 +1,16 @@
+// node
+// vendors
+import React from 'react';
+// project
+import Menu from './components/menu';
+
+import './styles.scss';
+
+const App = () => (
+    <div className='main-app'>
+        <h1>Hello, World!</h1>
+        <Menu />
+    </div>
+);
+
+export default App;
