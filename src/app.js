5796733c164e17b8f92100435c605516dc796d9d
Marcelo <marcelo.bohn@consyste.com.br>
2016-10-14 16:50:43 -0300

    insert test component react-bootstrap in base component

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ import React from 'react';
 // project
 import Menu from './components/menu';
 
+import { Button } from 'react-bootstrap';
+
 import './styles.scss';
 
 const App = () => (
   <div className='main-app'>
     <h1>Hello, World!</h1>
     <Menu />
+    <Button bsStyle="success">Success</Button>
   </div>
 );
 
