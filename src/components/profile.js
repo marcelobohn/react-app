4b979416f249385eb2c23a4493a2179dd4db3298
Marcelo <marcelo.bohn@consyste.com.br>
2016-10-17 17:50:31 -0200

    improve actions

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,23 +1,41 @@
 import React from 'react';
 
-import { Button } from 'react-bootstrap';
+import { Button, Checkbox } from 'react-bootstrap';
 
 import './styles.scss';
 
-function binding(fn, ...args) {
-  return fn.bind(fn, ...args);
-}
-
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false,
+      count: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChangeSelection = this.handleChangeSelection.bind(this);
+  }
   handleClick() {
-    alert('clicked');
+    global.console.log('button clicked');
+    this.setState({
+      showDetails: true,
+    });
+  }
+  handleChangeSelection() {
+    this.setState({
+      showDetails: !this.state.showDetails,
+      count: this.state.count + 1,
+    });
   }
   render() {
+    const { showDetails } = this.state;
     let name = this.props.name;
     return (
       <div className='profile'>
         <h1>{name}</h1>
-        <Button bsStyle='success' onClick={binding(this.handleClick)}>Success</Button>
+        <Checkbox checked={showDetails} onChange={this.handleChangeSelection}>
+          Mostrar detalhes (<span>{this.state.count}</span>)
+        </Checkbox>
+        <Button bsStyle='success' onClick={this.handleClick}>Success</Button>
       </div>
     );
   }
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
+import './styles.scss';
+
 function Welcome() {
   return (
     <Jumbotron>
